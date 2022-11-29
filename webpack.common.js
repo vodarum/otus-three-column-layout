ecbd70adc17acaa195326961a235578d86cf67f7
MAGNAT\ravshan_mu <muradov.rv@magnat.ru>
2022-11-29 23:51:15 +0300

    Add project settings

---
+++ webpack.common.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    filename: "main.js",
+    clean: true,
+    assetModuleFilename: "images/[name][ext]",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+};
